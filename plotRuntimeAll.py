df9ef01823465631f7c44155a472db26ed8db10e
Adrian <adriangrzegorczyk90@gmail.com>
2023-01-22 22:58:23 +0100

    Add fix data

--- plotRuntimeAll.py
+++ plotRuntimeAll.py
@@ -6,7 +6,7 @@ algo_points_y = np.array([3.5, 8.5, 82, 824.6, 6567.8])
 plt.plot(algo_points_x, algo_points_y)
 
 rounding_points_x = np.array([10, 20, 50, 100, 200])
-rounding_points_y = np.array([0.1, 0.5, 2.5, 16.2, 163.2])
+rounding_points_y = np.array([0.1, 0.5, 2.5, 16.2, 163.1])
 plt.plot(rounding_points_x, rounding_points_y)
 
 shift_points_x = np.array([10, 20, 50, 100, 200])
--- plotRuntimeShiftRounding.py
+++ plotRuntimeShiftRounding.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 rounding_points_x = np.array([10, 20, 50, 100, 200])
-rounding_points_y = np.array([0.1, 0.5, 2.5, 16.2, 163.2])
+rounding_points_y = np.array([0.1, 0.5, 2.5, 16.2, 163.1])
 plt.plot(rounding_points_x, rounding_points_y)
 
 shift_points_x = np.array([10, 20, 50, 100, 200])
